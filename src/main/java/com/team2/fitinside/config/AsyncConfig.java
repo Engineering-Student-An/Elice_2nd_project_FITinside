2ba31d9ee6f0fe0121a3d44273d61ca19d9c5ed4
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-01-19 21:56:08 +0900

    refactor: ThreadPoolTaskExecutor 사용하게 변경

--- src/main/java/com/team2/fitinside/config/AsyncConfig.java
+++ src/main/java/com/team2/fitinside/config/AsyncConfig.java
@@ -3,7 +3,20 @@ package com.team2.fitinside.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2); // 최소 스레드 수
+        executor.setMaxPoolSize(10); // 최대 스레드 수
+        executor.setQueueCapacity(100); // 대기열 크기
+        executor.initialize();
+        return executor;
+    }
 }
