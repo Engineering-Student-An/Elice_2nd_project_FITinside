5b4030c31afa2606585e7875eda13e019f7eeadb
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-01-24 13:52:54 +0900

    fix: 배포 오류 해결을 위한 cors 설정

--- src/main/java/com/team2/fitinside/config/WebConfig.java
+++ src/main/java/com/team2/fitinside/config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("https://fit-inside.netlify.app") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 쿠키 및 인증 헤더 포함 허용
     }
 }
