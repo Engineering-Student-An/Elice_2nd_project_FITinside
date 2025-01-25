804381980ed453d2ce16ee1f5fa81ffc1d0d79e9
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-01-25 20:11:32 +0900

    refactor: oauth2 관련 uri 수정

---
+++ src/main/java/com/team2/fitinside/oath/controller/OAuth2Controller.java
@@ -0,0 +1,19 @@
+package com.team2.fitinside.oath.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class OAuth2Controller {
+
+    @GetMapping("/login/oauth2/code/google")
+    public String googleLoginCallback(RedirectAttributes redirectAttributes) {
+        // 인증 후 처리 로직
+
+        // API Gateway URL로 리디렉션
+        String apiGatewayUrl = "https://obpedvusnf.execute-api.ap-northeast-2.amazonaws.com/login/oauth2/code/google";
+
+        return "redirect:" + apiGatewayUrl;
+    }
+}
